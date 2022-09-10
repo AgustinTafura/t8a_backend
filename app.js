efa3ae820eb03447f049effeaa19e2e5d4c62948
AgustinTafura <agustintafura@hotmail.com>
2022-09-10 17:11:15 -0300

    error handler

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 var cors = require('cors')
-
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -21,5 +21,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+app.use((req, res, next) => {
+    next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+    const {name, message} = err
+    res.status(err.status || 500);
+    res.json({name,message})
+    
+});
+
 
 module.exports = app;
